ea8f2efb97ee0fd0d35c0dc5adcb766974da79c2
Ben-Ferreira <benjamin@ferreira-Family.com>
2022-06-27 15:35:44 -0500

    deploy

---
+++ precache-manifest.e9e9bd24422cb07d89a589a5a7f7937a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f71f8a829eddd0d46bc1",
+    "url": "/static/css/main.54efc846.chunk.css"
+  },
+  {
+    "revision": "f71f8a829eddd0d46bc1",
+    "url": "/static/js/main.f71f8a82.chunk.js"
+  },
+  {
+    "revision": "6d901b3e33806cc9ca52",
+    "url": "/static/js/1.6d901b3e.chunk.js"
+  },
+  {
+    "revision": "47672b07f3432a6ea327",
+    "url": "/static/js/2.47672b07.chunk.js"
+  },
+  {
+    "revision": "2600f86dfe95919d6557",
+    "url": "/static/js/runtime~main.2600f86d.js"
+  },
+  {
+    "revision": "6527891795c5ec9ccbf1e325b9ccea32",
+    "url": "/index.html"
+  }
+];
